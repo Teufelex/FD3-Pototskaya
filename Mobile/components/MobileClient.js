ac3ab9ef72b515e25319b3ebd1373397561ad5f4
felix <gloomycoke@gmail.com>
2021-06-07 22:38:21 +0300

    init

---
+++ Mobile/components/MobileClient.js
@@ -0,0 +1,111 @@
+﻿import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import { companyEvents } from './events';
+
+import './MobileClient.css';
+
+class MobileClient extends React.PureComponent {
+
+  static propTypes = {
+    user: PropTypes.shape({
+      fam: PropTypes.string.isRequired,
+      im: PropTypes.string.isRequired,
+      otch: PropTypes.string.isRequired,
+      id: PropTypes.number.isRequired,
+      balance: PropTypes.number.isRequired,
+    }),
+  };
+
+  state = {
+    user: this.props.user,
+    workMode: 1, // 1 - read, 2 - edit
+  };
+
+  famValue = React.createRef();
+
+  imValue = React.createRef();
+
+  otchValue = React.createRef();
+
+  balanceValue = React.createRef();
+
+  editPressed = () => {
+    this.setState({workMode: 2});
+  }
+
+  deletePressed = () => {
+    console.log("delete")
+  }
+
+  savePressed = (e) => {
+    this.setState({workMode: 1});
+  }
+
+  cancelPressed = () => {
+    this.setState({workMode: 1});
+  }
+
+  componentDidMount() {
+    console.log(this.famValue)
+  }
+
+  componentDidUpdate() {
+    console.log(this.famValue)
+  }
+
+  render() {
+    console.log("MobileClient id="+this.state.user.id+" render");
+
+    return (
+      <div className='MobileClient'>
+        {
+          (this.state.workMode === 1) ? 
+          (
+            <Fragment>
+              <span className='MobileClientFIO clientCell'>{this.state.user.fam + ""}</span>
+              <span className='MobileClientFIO clientCell'>{this.state.user.im + ""}</span>
+              <span className='MobileClientFIO clientCell'>{this.state.user.otch + ""}</span>
+              <span className='MobileClientBalance clientCell'>{this.state.user.balance}</span>
+              {
+              (this.state.user.balance > 0) ?
+              <span className='clientCell MobileClient__activeBalance'>active</span> :
+              <span className='clientCell MobileClient__blockedBalance'>blocked</span>
+              }
+              <div className='clientCell'>
+                <input type = "button" value = "Редактировать" onClick = {this.editPressed}/>
+              </div>
+              <div className='clientCell'> 
+                <input type = "button" value = "Удалить" onClick = {this.deletePressed}/>
+              </div>
+            </Fragment>
+          ) : 
+          (
+            <Fragment>
+              <input type = "text" defaultValue = {this.state.user.fam} className='clientCell' 
+              ref={this.famValue}/>
+              <input type = "text" defaultValue = {this.state.user.im} className='clientCell'
+              ref={this.imValue}/>
+              <input type = "text" defaultValue = {this.state.user.otch} className='clientCell'
+              ref={this.otchValue}/>
+              <input type = "number" defaultValue = {this.state.user.balance} className='clientCell'
+              ref={this.balanceValue}/>
+              {
+              (this.state.user.balance > 0) ?
+              <span className='clientCell MobileClient__activeBalance'>active</span> :
+              <span className='clientCell MobileClient__blockedBalance'>blocked</span>
+              }
+              <div className='clientCell'>
+                <input type = "button" value = "Сохранить" onClick = {this.savePressed}/>
+              </div>
+              <div className='clientCell'> 
+                <input type = "button" value = "Отмена" onClick = {this.cancelPressed}/>
+              </div>
+            </Fragment>
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default MobileClient;
---
+++ Mobile/components/MobileCompany.js
@@ -0,0 +1,84 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+import MobileClient from './MobileClient';
+import { companyEvents } from './events';
+
+import './MobileCompany.css';
+
+class MobileCompany extends React.PureComponent {
+
+  static propTypes = {
+    clients:PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        fam: PropTypes.string.isRequired,
+        im: PropTypes.string.isRequired,
+        otch: PropTypes.string.isRequired,
+        balance: PropTypes.number.isRequired,
+      })
+    ),
+  };
+
+  state = {
+    initialClientsList: this.props.clients,
+    workMode: 1, //1-Velcom, 2-MTC
+    clients: this.props.clients,
+  };
+
+  switchToMTC = () => {
+    this.setState({workMode: 2});
+  }
+
+  switchToVelcom = () => {
+    this.setState({workMode: 1});
+  }
+
+  componentDidMount = () => {
+    //voteEvents.addListener('EAnswerClicked',this.answerSelected);
+    //voteEvents.addListener('EFreeAnswerTextChanged',this.freeAnswerTextChanged);
+  };
+
+  componentWillUnmount = () => {
+    //voteEvents.removeListener('EAnswerClicked',this.answerSelected);
+    //voteEvents.removeListener('EFreeAnswerTextChanged',this.freeAnswerTextChanged);
+  };
+  
+  render() {
+    console.log("MobileCompany render");
+
+    let clients = [];
+    this.state.clients.forEach(b => clients.push(<MobileClient user = {b} key = {b.id} event = {this.events}/>));
+
+    return (
+      <div className= 'MobileCompany'>
+        <div className = 'MobileCompany__controlButtonsWrap'>
+          <div>
+            <input type = "button" value = "МТС" onClick = {this.switchToMTC}/>
+            <input type = "button" value = "Velcom" onClick = {this.switchToVelcom}/>
+          </div>
+          <div>Компания: {(this.state.workMode === 1) ? "Velcom" : "МТС"}</div>
+        </div>
+        <div className = "MobileCompany__buttonsWrap">
+          <input type = "button" value = "Все"/>
+          <input type = "button" value = "Активный"/>
+          <input type = "button" value = "Заблокированные"/>
+        </div>
+        <div className = "MobileCompany__clientsWrap">
+          <div className = "clientsWrap__header">
+            <span className = "header__cell">Фамилия</span>
+            <span className = "header__cell">Имя</span>
+            <span className = "header__cell">Отчество</span>
+            <span className = "header__cell">Баланс</span>
+            <span className = "header__cell">Статус</span>
+            <span className = "header__cell">Редактировать</span>
+            <span className = "header__cell">Удалить</span>
+          </div>
+          {clients}
+        </div>
+        <input type = "button" value = "Добавить клиента"/>
+      </div>
+    );
+  }
+}
+
+export default MobileCompany;
